59882894b1b50270c4b237f027d24acf23ea6ee2
Kravets Vlad <meterlinkv@gmail.com>
2022-08-29 16:05:46 +0300

    message

--- src/Main/Java/com/hillel/kravets/lessons/homework3/Main.java
+++ src/Main/Java/com/hillel/kravets/lessons/homework3/Main.java
@@ -3,5 +3,13 @@ package com.hillel.kravets.lessons.homework3;
 public class Main {
     public static void main(String[] args) {
 
+        int a = 30;
+        int b = 50;
+        int h = 15;
+        int volume = a * b * h;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+
+        int length = 2 * (a + b);
+        System.out.println("Периметр паралелепіпеда = " + length);
     }
 }
