e11dd0b3c1bd9902a403303a74fb4693392ccc73
Kravets Vlad <meterlinkv@gmail.com>
2022-08-29 14:23:49 +0300

    message

---
+++ src/Main/Java/com/hillel/kravets/lessons/homework2/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.kravets.lessons.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double longitude = 48.4500000;
+
+        double latitude = 34.9833300;
+
+        System.out.println("Dnipro" + " " + longitude + "\u00B0" + latitude + "\u00B0" );
+
+    }
+}
---
+++ src/Main/Java/com/hillel/kravets/lessons/homework3/Main.java
@@ -0,0 +1,7 @@
+package com.hillel.kravets.lessons.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
